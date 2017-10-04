082206123bb3908d39c5e0fc5b5d3af3558040cc
ymwchina <yumuwu@live.cn>
2017-10-04 17:57:10 +0800

    format for date.

--- src/main/java/com/licai/model/User.java
+++ src/main/java/com/licai/model/User.java
@@ -1,8 +1,16 @@
 package com.licai.model;
 
+import java.util.Date;
+
 public class User {
     private int id;
     private String username;
+    private String password;
+    private Date createtime;
+    private Date lastmodifytime;
+    private String mobile;
+    private String email;
+    private String nickname;
 
     public int getId() {
         return id;
@@ -20,4 +28,51 @@ public class User {
         this.username = username;
     }
 
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Date getCreatetime() {
+        return createtime;
+    }
+
+    public void setCreatetime(Date createtime) {
+        this.createtime = createtime;
+    }
+
+    public Date getLastmodifytime() {
+        return lastmodifytime;
+    }
+
+    public void setLastmodifytime(Date lastmodifytime) {
+        this.lastmodifytime = lastmodifytime;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
 }
--- src/main/webapp/WEB-INF/views/user/userList.jsp
+++ src/main/webapp/WEB-INF/views/user/userList.jsp
@@ -1,14 +1,31 @@
 <%@taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%@ taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" pageEncoding="UTF-8" isELIgnored="false" import="java.util.*" %>
 <table align="center" border="1" cellspacing="0">
     <tr>
         <td>id</td>
-        <td>name</td>
+        <td>username</td>
+        <td>password</td>
+        <td>createtime</td>
+        <td>lastmodifytime</td>
+        <td>mobile</td>
+        <td>email</td>
+        <td>nickname</td>
+        <td>Edit</td>
+        <td>Delete</td>
     </tr>
     <c:forEach items="${users}" var="c" varStatus="st">
         <tr>
             <td>${c.id}</td>
             <td>${c.username}</td>
+            <td>${c.password}</td>
+            <td><fmt:formatDate value="${c.createtime}" pattern="yyyy-MM-dd  HH:mm:ss" /></td>
+            <td><fmt:formatDate value="${c.lastmodifytime}" pattern="yyyy-MM-dd  HH:mm:ss"/></td>
+            <td>${c.mobile}</td>
+            <td><c:if test="${not empty c.email}">${c.email}</c:if></td>
+            <td><c:if test="${not empty c.nickname}">${c.nickname}</c:if> </td>
+            <td><a href="/user/EditUser?id=${c.id}">edit</a> </td>
+            <td><a href="/user/DeleteUser?id=${c.id}">delete</a></td>
         </tr>
     </c:forEach>
 </table>
